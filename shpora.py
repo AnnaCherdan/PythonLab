914baab7aeea8870fd4a0a9bd16be37587edf3c0
AnnaCherdan <anacher@mail.ru>
2022-04-07 00:30:58 +0700

    About something else.

--- shpora.py
+++ shpora.py
@@ -1,12 +1,12 @@
 # <class 'NoneType'>
 # <class 'int'>
 # <class 'float'>
-# <class 'complex'>
+# <class 'complex'
 # <class 'str'>
 # <class 'list'()>
 # <class 'tuple'>
-# <class 'set'>
-# <class 'dict'>
+# <class 'set'> Множества {'a', 'b', 'c'}> Для избавления от дублей.
+# <class 'dict'{'key':'value'}>
 # <class 'bool'>
 # ОПЕРАТОРЫ СРАВНЕНИЯ
 # if

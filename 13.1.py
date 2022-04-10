71ea6df19581a68bfc2d49dbd23ea26f88357b69
AnnaCherdan <100855612+AnnaCherdan@users.noreply.github.com>
2022-04-10 16:09:55 +0700

    Сравнение, побитовые логические.

--- 13.1.py
+++ 13.1.py
@@ -4,42 +4,42 @@
 # Тернарные Операторы- возвращают свой второй или третий операнд в
 # зависимости от значения логического выражения, заданного первым операндом.
 # ОПЕРАТОРЫ СРАВНЕНИЯ. Результат всегды булево значение.
-# print(10 < 5)
-# print(10 > 5)
-# print(10 <= 5)
-# print(10 >= 5)
-# print(10 <= (5 * 2))
-# print(3 == 5)
-# print(3 != 5)
-# print('-' * 50)
-#
-#
-# print(25 ^ 3)
-#
-# print('-' * 50)
-# cond1 = 0 < 1
-# cond2 = 1 < 4
-# print(cond1 and cond2)
-# print('-' * 20)
-# cond1 = 0 > 1
-# print(cond1)
-# cond2 = 1 < 4
-# print(cond2)
-# print(cond1 and cond2)
-# print('-' * 20)
-# cond1 = 0 > 1
-# print(cond1)
-# cond2 = 1 > 4
-# print(cond2)
-# print(cond1 and cond2)
-# print('-' * 20)
-# name1 = "Ann"  # Регистр имеет значение!
-# name2 = 'Elen'
-# cond1 = 'A' in name1
-# print(cond1)
-# cond2 = 'A' in name2
-# print(cond2)
-# print(cond1 and cond2)
+print(10 < 5)
+print(10 > 5)
+print(10 <= 5)
+print(10 >= 5)
+print(10 <= (5 * 2))
+print(3 == 5)
+print(3 != 5)
+print('-' * 50)
+
+
+print(25 ^ 3)
+
+print('-' * 50)
+cond1 = 0 < 1
+cond2 = 1 < 4
+print(cond1 and cond2)
+print('-' * 20)
+cond1 = 0 > 1
+print(cond1)
+cond2 = 1 < 4
+print(cond2)
+print(cond1 and cond2)
+print('-' * 20)
+cond1 = 0 > 1
+print(cond1)
+cond2 = 1 > 4
+print(cond2)
+print(cond1 and cond2)
+print('-' * 20)
+name1 = "Ann"  # Регистр имеет значение!
+name2 = 'Elen'
+cond1 = 'A' in name1
+print(cond1)
+cond2 = 'A' in name2
+print(cond2)
+print(cond1 and cond2)
 print('-' * 20)
 name1 = "Ann"  # Регистр имеет значение!
 name2 = 'Elen'

990d220e2649e209b679e17e241343e2b05c4924
AnnaCherdan <100855612+AnnaCherdan@users.noreply.github.com>
2022-04-17 11:20:50 +0700

    About palindrome and outside 'c'.

--- 14.1_8.1.py
+++ 14.1_8.1.py
@@ -41,3 +41,72 @@ outside_result = pow_func(3)
 print(outside_result)
 # 9
 
+# функцию, которая проверяет, является ли данная строка палиндромом или нет, и возвращает результат проверки.
+
+print('My Bad Var')
+# def check_palindrome(tx):
+#    tx = str(list(input())
+#             for n in tx:
+#                print(n, )
+
+print('Skill Var')
+
+
+def check_palindrome(str_):
+    str_ = str_.lower()    # Переводим в нижний регистр.
+    str_ = str_.replace(" ", "")  # заменяем пробелы на ничто.
+
+    if str_ == str_[::-1]:
+        return True    # Если строка равна строке с конца до начала с обратным шагом.
+    else:
+        return False
+
+
+check_palindrome("test")  # False
+check_palindrome("Кит на море не романтик")  # True
+
+# не могли сразу распечатать переменную inside_result вне тела функции
+
+
+print('Skill BAD Var')
+
+
+# def pow_func(base, n=2):
+#    inside_result = base ** n
+#    return inside_result
+#
+# print(inside_result)
+# # ----> 5 print(inside_result)
+# #       6 # 9
+
+
+print('My GOOD Var')
+
+
+def pow_func(base, n=2):
+    inside_result = base ** n
+    return inside_result
+
+
+c = pow_func(6, 9)
+print(c)
+# ----> 5 print(inside_result)
+#       6 # 9
+
+
+print('My GOOD Var')
+
+
+def check_palindrome(str_):
+    str_ = str_.lower()  # Переводим в нижний регистр.
+    str_ = str_.replace(" ", "")  # Заменяем пробелы на ничто.
+
+    if str_ == str_[::-1]:
+        return True  # Если строка равна строке с конца до начала с обратным шагом.
+    else:
+        return False
+
+
+print(check_palindrome("test"))  # False
+print(check_palindrome("А роза упала на лапу Азора"))  # True
+print(check_palindrome("А пороо Влдлд"))

12a94318452c85b611b5e64fb9588b8051737165
AnnaCherdan <100855612+AnnaCherdan@users.noreply.github.com>
2022-09-22 19:19:55 +0700

    Date types

---
+++ 12.veb.py
@@ -0,0 +1,27 @@
+import math
+name = "Анютка"
+age = 41
+# print("Привет, " + name + ". Тебе ", age, " год! Поздравляю, смерть близка...")
+print(f'Привет, {name}. Тебе {age} год! Поздравляю, смерть близка...')
+# # camel case - userName, только в названиях класса!
+# userName = "Аннушка"
+# print("Зато, " + userName + ", твоё желание саморазвития радует!")
+# # snake_case - user_name
+# age1 = 15
+# name1 = "Саши"
+# age2 = age + age1
+# print('А твой, ' + name + ", и " + name1 + " общий возраст равен", age2)
+# print(bin(120))
+
+print(name[5]) # Выделение символа по индексу.
+print(name[1:4:2]) # Срез строки [start:stop:step]
+
+# print(int(math.pow(2, 6))) # Возведение 2 в степень 6.
+# print(math.sqrt(25)) # Извлечение квадратного корня.
+# print(math.pi)
+# print(round(math.pi, 2)) # Округление Пи на 2 знака полсле запятой.
+# print(round(10/3, 3))
+# f = 5 // 2
+# print(f)
+
+
